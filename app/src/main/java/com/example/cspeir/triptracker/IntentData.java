bdb205a39752bf453e10899f1186f87eee88f2e1
cspeir <cospe18@hotmail.com>
2017-12-02 16:59:51 -0500

    fixed registration error

--- app/src/main/java/com/example/cspeir/triptracker/IntentData.java
+++ app/src/main/java/com/example/cspeir/triptracker/IntentData.java
@@ -12,4 +12,5 @@ public interface IntentData {
     String EXTRA_TRIP_END_DATE = "org.pltw.examples.triptracker.TRIP_END_DATE";
     String EXTRA_TRIP_PUBLIC = "org.pltw.examples.triptracker.TRIP_PUBLIC";
     String EXTRA_TRIP_PUBLIC_VIEW = "org.pltw.examples.triptracker.TRIP_PUBLIC_VIEW";
+    String EXTRA_TRIP_OWNER_ID = "org.pltw.examples.triptracker.TRIP_OWNER_ID";
 }
